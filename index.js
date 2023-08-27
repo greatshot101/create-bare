5676363d406eeac6e14a0e26360e57ce7e3f941c
WaterTheWolf <87424826+WaterWolf5918@users.noreply.github.com>
2023-08-27 17:04:58 -0400

    https support

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-import { createServer } from "node:http";
-import { createBareServer } from "@tomphttp/bare-server-node";
-import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { createServer } from 'node:https';
+import { createServer as createHTTP } from 'node:http';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import express from 'express';
-
-const bare = createBareServer("/bare/");
+import * as fs from 'fs';
+import * as process from 'process';
+const bare = createBareServer('/bare/');
 const app = express();
 
 app.use(express.static('static'));
-app.use("/uv/", express.static('static/js/uv/'));
-
-const server = createServer();
-
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
+app.use('/uv/', express.static('static/js/uv/'));
+const httpServer = createHTTP()
+const server = createServer({
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CERT)
 });
 
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeRequest(req, res);
+    } else {
+        app(req, res);
+    }
+});
+
+server.on('upgrade', (req, socket, head) => {
+    if (bare.shouldRoute(req)) {
+        bare.routeUpgrade(req, socket, head);
+    } else {
+        socket.end();
+    }
+});
+
+httpServer.on('request', (req,res) => {
+    app(req, res);
+    // res.writeHead(200)
+    // res.end("<h1>ERROR: This server only supports https<h1/>")
+})
+
+httpServer.listen({
+    port: 80,
 });
 
 server.listen({
-  port: (process.env.PORT || 80),
+    port: 443,
 });
