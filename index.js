b91442e1758e00c1ea5cd4bbc3c2033bb97ea669
greatshot101 <116977958+greatshot101@users.noreply.github.com>
2023-11-01 21:09:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-// import { createServer } from 'node:https';
-import { createServer as createHTTP } from 'node:http';
-import { createBareServer } from '@tomphttp/bare-server-node';
-import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+import { createServer } from 'node:http'; // Import the 'http' module
 import express from 'express';
-import * as fs from 'fs';
-import * as process from 'process';
+import createBareServer from '@tomphttp/bare-server-node';
+import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+
 const bare = createBareServer('/bare/');
 const app = express();
 
+app.use(express.static('static'));
 app.use('/uv/', express.static('static/js/uv/'));
-const httpServer = createHTTP()
-// const server = createServer({
-//     key: fs.readFileSync(process.env.KEY),
-//     cert: fs.readFileSync(process.env.CERT)
-// });
+
+const server = createServer();
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeRequest(req, res);
-    } else {
-        app(req, res);
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
 });
 
 server.on('upgrade', (req, socket, head) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
-    }
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
-httpServer.on('request', (req,res) => {
-    app(req, res);
-    // res.writeHead(200)
-    // res.end("<h1>ERROR: This server only supports https<h1/>")
-})
-
-httpServer.listen({
-    port: 80,
-});
-
-// server.listen({
-//     port: 443,
-// });
+export default async (req, res) => {
+  // Handle incoming HTTP requests using Vercel serverless functions
+  server.emit('request', req, res);
+};
